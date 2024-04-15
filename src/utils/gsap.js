a1eb22d4f39495ccfb593978f74c94c79ce6da3c
Liam Marega <86364396+LiamMarega@users.noreply.github.com>
2024-04-15 17:39:09 -0300

    Agrega estilos y animaciones a la tarjeta de transmisión en StreamCard.astro

--- src/utils/gsap.js
+++ src/utils/gsap.js
@@ -5,7 +5,6 @@ gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline();
 
 ScrollTrigger.create({
-  markers: true,
   animation: tl,
   trigger: '#main_home',
   start: '0% top',
@@ -15,18 +14,18 @@ ScrollTrigger.create({
 });
 
 tl.from('#twitch-embed', {
-  yPercent: 50, // Ajustado a 0 para mantener el elemento centrado
-  opacity: 0.5,
-  marginLeft: '20vw',
+  yPercent: 40, // Ajustado a 0 para mantener el elemento centrado
+  opacity: 0.7,
+  //   marginLeft: '18vw',
   width: '70vw',
-  scale: 0.5,
+  scale: 0.7,
   delay: 0.5,
   ease: 'none',
 });
 tl.from('#twitch-chat', {
-  yPercent: 50, // Ajustado a 0 para mantener el elemento centrado
-  opacity: 0,
-  scale: 0.5,
+  yPercent: 40, // Ajustado a 0 para mantener el elemento centrado
+  display: 'none',
+  scale: 0.7,
   duration: 0.5,
 
   ease: 'none',
